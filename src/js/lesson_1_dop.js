1a7e59745477d3eed0168d8edb6437e45f4ac889
krafik <alexeym4l@mail.ru>
2018-11-16 20:32:51 +0300

    first lesson complicated

---
+++ src/js/lesson_1_dop.js
@@ -0,0 +1,5 @@
+let str = (33721).toString();
+let mult = (str[0] * str[1] * str[2] * str[3] * str[4]);
+let pow = (mult ** 3).toString();
+
+document.write("first 3 numeral after pow mult - "+pow[0]+pow[1]+pow[2]);
